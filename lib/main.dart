9d35f8d770372985e48d10e113e092cae76e8404
Waako Shadidu <shadiduwaako@gmail.com>
2024-05-24 15:39:52 +0300

    changing string of first text object

--- lib/main.dart
+++ lib/main.dart
@@ -37,10 +37,11 @@ class MyHomePage extends StatelessWidget {
     return Scaffold(
       body: Column(
         children: [
-          Text('A random idea:'),
+          Text('A random AWESOME idea:'),
           Text(appState.current.asLowerCase),
         ],
       ),
     );
   }
 }
+
