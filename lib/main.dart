c0922db7011bcae983cad7cd2500e12b6dd08ab0
Waako Shadidu <shadiduwaako@gmail.com>
2024-05-25 19:37:54 +0300

    made navigation rail resposive

--- lib/main.dart
+++ lib/main.dart
@@ -49,6 +49,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  var selectedIndex = 0; 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -67,9 +68,12 @@ class _MyHomePageState extends State<MyHomePage> {
                   label: Text('Favorites'),
                 ),
               ],
-              selectedIndex: 0,
+              selectedIndex: selectedIndex,
               onDestinationSelected: (value) {
-                print('selected: $value');
+                setState(() {
+                  selectedIndex = value;
+                });
+
               },
             ),
           ),
@@ -84,6 +88,7 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 }
+
 
 
 class GeneratorPage extends StatelessWidget {
